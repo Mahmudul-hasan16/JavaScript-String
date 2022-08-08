03d337cab04e52c292a9029479de5ec04f2eea16
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-08-09 02:16:10 +0600

    word-reverse add

---
+++ word-reverse.js
@@ -0,0 +1,26 @@
+
+
+function reverseWords(str) {
+
+
+    const words = str.split(' ');
+    console.log(words);
+    const result = [];
+
+
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        const element = words[i];
+        result.push(element);
+
+    }
+
+
+    console.log(result);
+    const reversed = result.join(' ');
+    return reversed;
+}
+
+const myString = 'I am a good boy'
+const reverseWord = reverseWords(myString);
+console.log(reverseWord);
