e628981928d29af4fc0531c6d8759dc88433e3cd
Mahmudul-hasan16 <m.hasan5979@gmail.com>
2022-08-09 10:49:46 +0600

    fibo added

---
+++ fibonacci-series.js
@@ -0,0 +1,19 @@
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+/*
+    Fibonacci series logic
+    fibo[3] = fibo[2] + fibo[1]
+    fibo[4] = fibo[3] + fibo[2]
+    fibo[5] = fibo[4] + fibo[3]
+    fibo[n] = fibo[n-1] + fibp[n-2]
+    fibo[i] = fibo[i-1] + fibo[i-2]
+
+
+
+*/
+
+const fibo = [0, 1];
+for (let i = 2; i < 13; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+}
+console.log(fibo);
